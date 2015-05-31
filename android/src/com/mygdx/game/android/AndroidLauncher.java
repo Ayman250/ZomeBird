ea9056b555a81b121eef84cd8c11d55ce90c478d
Ayman250 <Aymanelmubark@gmail.com>
2015-05-30 20:35:30 -0500

    sdoifjisfo

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -14,6 +14,6 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 
 		initialize(new ZBGame(), config);
-
+//POGMAEOIJGo
 	}
 }
